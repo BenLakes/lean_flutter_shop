df1cac9f4a3fdcc0078cdbf2039c4b2ca623d862
linanzhi <604429058@qq.com>
2020-01-14 15:02:37 +0800

    添加底部导航栏与相应页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,21 @@
 import 'package:flutter/material.dart';
+import './pages/index_page.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var card = Card(
-      child: Column(
-        children: <Widget>[
-          ListTile(
-             title: Text(
-               "福建省厦门市湖里区",
-               style: TextStyle(fontWeight: FontWeight.w500),
-               ), 
-               subtitle: Text("lee: 1512312312"),
-               leading: Icon(Icons.add_call, color: Colors.lightBlue),
-          ), 
-          // 分割线
-          Divider(),
-          ListTile(
-             title: Text(
-               "福建省厦门市思明区",
-               style: TextStyle(fontWeight: FontWeight.w500),
-               ), 
-               subtitle: Text("lee: 1512312312"),
-               leading: Icon(Icons.add_call, color: Colors.lightBlue),
-          ), 
-          Divider(),
-          ListTile(
-             title: Text(
-               "福建省厦门市集美区",
-               style: TextStyle(fontWeight: FontWeight.w500),
-               ), 
-               subtitle: Text("lee: 1512312312"),
-               leading: Icon(Icons.add_call, color: Colors.lightBlue),
-          ), 
-          Divider(),
-          ListTile(
-             title: Text(
-               "福建省厦门市同安区",
-               style: TextStyle(fontWeight: FontWeight.w500),
-               ), 
-               subtitle: Text("lee: 1512312312"),
-               leading: Icon(Icons.add_call, color: Colors.lightBlue),
-          ), 
-          Divider(),
-          ListTile(
-             title: Text(
-               "福建省厦门市翔安区",
-               style: TextStyle(fontWeight: FontWeight.w500),
-               ), 
-               subtitle: Text("lee: 1512312312"),
-               leading: Icon(Icons.add_call, color: Colors.lightBlue),
-          ), 
-          Divider(),
-        ],
-      ),
-    );
-      
-    return MaterialApp(
-      title: "Hello Lee Demo",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("垂直方向布局 Demo"),
+    // 使用Container 作为根widget是为了 更好扩展 padding/margin
+    return Container(
+        child: MaterialApp(
+          title: '商城学习',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: Colors.pink
+          ),
+          home: IndexPage(),
         ),
-        body: Center(
-          child: card,
-        ),
-      ),
     );
   }
 }
