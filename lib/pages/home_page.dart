9d18bb2ba58bc1e41902b0da2a4f1f83a2fee849
linanzhi <604429058@qq.com>
2020-01-15 10:33:11 +0800

    添加Dio网络请求库 EasyMock 添加接口

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(child: Text('商城首页'),),
     );
+  }
+
+  void getHttp()async{
+    try {
+      Response response ;
+      var data = {'name': 'Lee'};
+       response = await Dio().get(
+        "https://www.easy-mock.com/mock/5e1dcdd5fe6d23409285a687/example/getTest?name=123123",
+        //  queryParameters:data
+      ); 
+      print(response);
+    } catch (e) {
+      return print(e);
+    }
   }
 }
