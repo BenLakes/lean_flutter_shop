fb011fc1920c72579a38da70eae795db69c93229
linanzhi <604429058@qq.com>
2020-01-15 12:52:56 +0800

    添加接口定义  编写获取首页接口数据

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-// 定义请求头
-import '../config/httpHeaders.dart';
+import 'package:lean_flutter_shop/service/service_method.dart';
+import '../config/service_url.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,86 +9,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // 文本编辑控制器  可获得 输入值
-  TextEditingController typeController = TextEditingController();
+  
+  String homePageContent = '正在获取数据';
 
-  String showText = "欢迎你来到美好人间";
+  @override
+  void initState() {
+    getHomePageContent().then((val){
+       setState((){
+         homePageContent = val.toString(); 
+       }); 
+    }); 
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Scaffold(
-          appBar: AppBar(title: Text("美好人间"),),
-          body: SingleChildScrollView(
-            child:   Container(
-            height: 1000,
-            margin: EdgeInsets.only(top: 320),
-            child: Column(
-              children: <Widget>[
-                  TextField(
-                    controller: typeController,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(10.0),
-                      labelText: '美女类型',
-                      helperText: '请输入美女类型',
-                      border: OutlineInputBorder(),
-                      hintText: '请输入账号',
-                      prefixIcon: Icon(Icons.person),
-                    ),
-                  ),
-                  // 当需要调用接口时候  点击按钮
-                  RaisedButton(
-                    onPressed: _choiceAction,
-                    child: Text("选择完毕"),
-                  ),
-                  Text(
-                    showText,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-              ],
-            ),
-          ),
-          ),
-        
-       ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("学习商城")
+      ),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
+      ),
     );
   }
-  // 按钮事件
- void _choiceAction () async {
-   print("开始选择你喜欢的类型........");
-   if (typeController.text.toString() == '') {
-    //  显示提示
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(title: Text("美女类型不能为空"),)
-    );
-   } else {
-// 不为空请求网络
-     var res = await _postHttp(typeController.text.toString());
-     print("res === ${res}");
-     setState(() {
-       showText = res['data']['name'].toString();
-     });
-   }
- }
-  
-  Future _postHttp(String typeText) async {
-     try {
-       Response response;
-       var data = {'name': typeText};
-       var dio = Dio();
-       dio.options.headers = httpHeaders;
-       response = await dio.post('https://www.easy-mock.com/mock/5e1dcdd5fe6d23409285a687/example/postTest',
-       queryParameters: data);
-       print(response.data);
-       return response.data;
-     } catch (e) {
-       return print(e);
-     }
-
-
-
-  }
-
 }
